a44f04b5858b609d3093b921bbdf42a3005e7bc9
mh5458837@gmail.com <mh5458837@gmail.com>
2025-10-09 09:52:52 +0700

    Create API Auth: Create API Login & Login Test

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { serve } from '@hono/node-server';
 import { db } from './db/index.js';
 import { users } from './db/schema.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { setCookie } from 'hono/cookie';
 
 const app = new Hono();
 
@@ -11,6 +13,8 @@ app.get('/', (c) => {
     return c.html('<h1>Tim Pengembang</h1><h2>Muhammad</h2>')
 });
 
+
+// Register 
 app.post('/api/register', async (c) => {
     try {
         const { username, password } = await c.req.json();
@@ -22,6 +26,23 @@ app.post('/api/register', async (c) => {
         return c.json({ success: false, message: 'Registrasi Gagal' }, 404);
     }
 });
+
+// Login
+app.post('/api/login', async (c) => {
+    const { username, password } = await c.req.json();
+    const user = await db.query.users.findFirst({ where: (users, { eq }) => eq(users.username, username) });
+
+    if(!user) return c.json({ success: false, message: 'Username atau password salah' }, 401);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) return c.json({ success: false, message: 'Username atau Password salah'}, 401);
+
+    const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    setCookie(c, 'token', token, { httpOnly: true, sameSite: 'Lax', maxAge: 3600 });
+
+    return c.json({ success: true, message: 'Login Berhasil' });
+})
 
 // jalankan server
 const port = 5002;
