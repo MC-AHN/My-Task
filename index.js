8e11461f1593836de977ad211ddf6577538c9489
mh5458837@gmail.com <mh5458837@gmail.com>
2025-10-09 09:23:14 +0700

    Make API Auth: Create API Registrasi & Registrasi Test

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import 'dotenv/config';
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import { db } from './db/index.js';
+import { users } from './db/schema.js';
+import bcrypt from 'bcryptjs';
 
 const app = new Hono();
 
 app.get('/', (c) => {
     return c.html('<h1>Tim Pengembang</h1><h2>Muhammad</h2>')
+});
+
+app.post('/api/register', async (c) => {
+    try {
+        const { username, password } = await c.req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await db.insert(users).values({ username, password: hashedPassword }).returning({ id: users.id, username: users.username });
+
+        return c.json({ success: true, data: newUser[0] }, 201);
+    } catch (error) {
+        return c.json({ success: false, message: 'Registrasi Gagal' }, 404);
+    }
 });
 
 // jalankan server
